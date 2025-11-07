bfcc4bbc70811c15e2536977fce5d273d8f6ffc6
iresh-dev <thumanaitech@gmail.com>
2025-11-07 16:30:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@
   import react from "@vitejs/plugin-react";
 
   export default defineConfig({
-    base: "/iresh-dev.github.io/",
     plugins: [stringPlugin(), react()],
     assetsInclude: ["**/*.glb"],
     base: "/",
