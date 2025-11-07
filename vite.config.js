739d5da62e7944ceb424eb0a4aaaa2d4e020c95a
iresh-dev <thumanaitech@gmail.com>
2025-11-07 16:16:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import stringPlugin from "vite-plugin-string";
-import react from "@vitejs/plugin-react";
+  import { defineConfig } from "vite";
+  import stringPlugin from "vite-plugin-string";
+  import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [stringPlugin(), react()],
-  assetsInclude: ["**/*.glb"],
-  base: "/",
-});
+  export default defineConfig({
+    base: "/iresh-dev.github.io/",
+    plugins: [stringPlugin(), react()],
+    assetsInclude: ["**/*.glb"],
+    base: "/",
+  });
